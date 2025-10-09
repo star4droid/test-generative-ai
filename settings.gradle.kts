cd4007e613ebaca81ff5bb6f8554e3d42666fc50
Star4Droid <ans44029@gmail.com>
2025-10-10 00:11:45 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,22 +6,4 @@ pluginManagement {
     }
 }
 
-include(":admob:app",
-        ":firebase-ai:app",
-        ":analytics:app",
-        ":appdistribution:app",
-        ":auth:app",
-        ":config:app",
-        ":crash:app",
-        ":database:app",
-        ":dataconnect:app",
-        ":firestore:app",
-        ":functions:app",
-        ":internal:chooserx",
-        ":internal:lint",
-        ":internal:lintchecks",
-        ":inappmessaging:app",
-        ":messaging:app",
-        ":perf:app",
-        ":storage:app"
-)
+include(":firebase-ai:app")
